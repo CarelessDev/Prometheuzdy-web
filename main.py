b8be70e50d71c3037260f2b377b08781e7f6c319
WasinUddy <wasinsilakong@gmail.com>
2023-10-23 23:59:34 +0700

    allows CORS

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ except:
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.api.rest.router import router as rest_router
 from app.api.web.router import router as web_router
@@ -15,6 +17,18 @@ from app.api.web.router import router as web_router
 
 app = FastAPI()
 
+origins = [
+    "https://prometheuzdy.cloud",
+    "http://prometheuzdy.cloud",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mount the static files directory
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
